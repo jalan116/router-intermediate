d858393d3bb1e50b13111b1483ec4a3defb0b69f
jalan116 <jalan116@stu.southtexascollege.edu>
2021-03-14 22:44:28 -0500

    SignUp and Google Login

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,35 +1,47 @@
 <template>
-    <div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
-  <div class="flex-1 px-2 mx-2">
-    <span class="text-lg font-bold">
-            Login Example
-          </span>
-  </div> 
-  <div class="flex-none hidden px-2 mx-2 lg:flex">
-    <div class="flex items-stretch">
-      <router-link to="/" class="btn btn-ghost btn-sm rounded-btn">
-              Home
-            
-      </router-link> 
-      
-      <router-link v-if="!isAuthenticated" to="/login" class="btn btn-ghost btn-sm rounded-btn">
-              Login
-            
-      </router-link>
-      <div  v-else>
-        <router-link  to="/secret" class="btn btn-ghost btn-sm rounded-btn">
-              Secret Page
-            
+  <div class="navbar mb-2 shadow-lg bg-content-800 text-content-100">
+    <div class="flex-1 px-2 mx-2">
+      <span class="text-lg font-bold"> LoginExample </span>
+    </div>
+    <div class="flex-none flex px-2 mx-2">
+      <div class="flex">
+        <router-link
+          to="/"
+          class="btn btn-ghost btn-sm rounded-btn hover:text-content-100"
+        >
+          Home
         </router-link>
-        <button to="/login" class="btn btn-error btn-sm rounded-btn hover:text-content-100" @click="logout">
-              Logout
-            
-        </button>
-        
+        <div v-if="!isAuthenticated">
+          <router-link
+            to="/login"
+            class="btn btn-ghost btn-sm rounded-btn hover:text-content-100"
+          >
+            Login
+          </router-link>
+          <router-link
+            to="/signup"
+            class="btn btn-ghost btn-sm rounded-btn hover:text-content-100"
+          >
+            SignUp
+          </router-link>
+        </div>
+        <div v-else>
+          <router-link
+            to="/secret"
+            class="btn btn-ghost btn-sm rounded-btn hover:text-content-100"
+          >
+            Secret Page
+          </router-link>
+          <button
+            class="btn btn-error btn-sm rounded-btn hover:text-content-100"
+            @click="logout"
+          >
+            Logout
+          </button>
+        </div>
       </div>
     </div>
-  </div> 
-</div>
+  </div>
 </template>
 
 <script setup>
--- src/router.js
+++ src/router.js
@@ -3,13 +3,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import Secret from './views/Secret.vue'
 import Login from './views/Login.vue'
+import SignUp from './views/SignUp.vue'
 import NotFound from './views/NotFound.vue'
 
 import { isAuthenticated } from './helpers/useAuth'
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/secret',
+  {
+    path: '/secret',
     component: Secret,
     beforeEnter: (to, from) => {
       if (isAuthenticated.value) return true
@@ -17,6 +19,7 @@ const routes = [
     },
   },
   { path: '/login', component: Login },
+  { path: '/signup', component: SignUp },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ]
 
